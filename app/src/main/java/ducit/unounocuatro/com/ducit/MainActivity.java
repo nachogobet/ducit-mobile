f5e943eb1283ba5554514d5118a7763d474040c4
Ignacio Gobet <ignacio.gobet@plugtree.com>
2016-08-23 16:18:59 -0300

    second commit

---
+++ app/src/main/java/ducit/unounocuatro/com/ducit/MainActivity.java
@@ -0,0 +1,35 @@
+package ducit.unounocuatro.com.ducit;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TessBaseAPI baseApi = new TessBaseAPI();
+        baseApi.setDebug(true);
+        baseApi.init(Environment.getExternalStorageDirectory().toString(), "spa");
+        Bitmap bMap = BitmapFactory.decodeFile("/storage/emulated/0/Pictures/celu2.jpg");
+        baseApi.setImage(bMap);
+        String recognizedText = baseApi.getUTF8Text();
+        LinearLayout lView = new LinearLayout(this);
+
+        TextView myText = new TextView(this);
+        myText.setText(recognizedText);
+
+        lView.addView(myText);
+
+        setContentView(lView);
+        baseApi.end();
+    }
+}
